c2486cece6a694bc778d7a9ee7ef42e861ff3f61
Ukesh Dhakal <130669170+ukesh-dhakal@users.noreply.github.com>
2025-11-13 12:31:40 +0545

    Ensure Instagram footer icon is visible

---
+++ assets/styles/tailwindconfig.js
@@ -0,0 +1,17 @@
+if (typeof tailwind !== "undefined") {
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: "#111013",
+          secondary: "#a6abb4",
+          accent: "#fdc408",
+          surface: "#1c1b1e",
+        },
+        fontFamily: {
+          urbanist: ["Urbanist", "sans-serif"],
+        },
+      },
+    },
+  };
+}
